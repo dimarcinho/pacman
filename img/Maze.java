9ba86ccd5d8f1089bba26b5350b48bd0dc41c763
dimarcinho <marciozw9@hotmail.com>
2016-03-10 01:01:16 -0300

    Added files via upload

--- img/Maze.java
+++ img/Maze.java
@@ -7,35 +7,26 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.LinkedList;
+import objects.Dot;
+import objects.DotController;
+
 
 public class Maze {
     
     private ImageLoader i = new ImageLoader();
     private SpriteSheet ss = new SpriteSheet(i.load("maze_spritesheet2.png"));
     
-    public void Maze(){
+    private LinkedList <String> maze = new LinkedList<String>();
+    private String tempLine;
         
-    //BufferedReader txt = new BufferedReader(new FileReader("maze.txt"));    
+    private int xSS, ySS;
     
-    
-    //String line;
-    
-        
-    }
-    
-    public void update(){
-        
-    }
-    
-    public void draw(Graphics g2d){
-        
-        //rotina para desenhar o labirinto usando spritesheet
-        BufferedReader br = null;        
-        String tempLine;
-        
-        int xSS, ySS;
-        int k = 3;
+    public Maze(){
+                
+        //Lê o arquivo e armazena os dados num vetor
+        BufferedReader br = null;
+        int k = 3; //deixa 3 tiles em branco para o "menu" do jogo
         
         try {
             br = new BufferedReader(new FileReader("C:/Users/Marcio/Documents/NetBeansProjects/PacMan/src/img/maze.txt"));
@@ -43,37 +34,80 @@ public class Maze {
             tempLine = br.readLine();
             
             do  {
-
-                for(int j = 0; j < 28; j++){
-                    
-                    if(tempLine.substring(2*j, 2*j+2).equals("dd")){
-                        g2d.drawImage(ss.crop(6, 0, 18, 18), j*18, k*18, null); //6,0
-                    } else if(tempLine.substring(2*j, 2*j+2).equals("up")){
-                        g2d.drawImage(ss.crop(6, 1, 18, 18), j*18, k*18, null); //6,1
-                    } else if(tempLine.substring(2*j, 2*j+2).equals("xx")){
-                        g2d.drawImage(ss.crop(7, 0, 18, 18), j*18, k*18, null); //7,0
-                    } else {
-                        xSS = Integer.parseInt(tempLine.substring(2*j, 2*j+1));
-                        ySS = Integer.parseInt(tempLine.substring(2*j+1, 2*j+2));                    
-                        g2d.drawImage(ss.crop(xSS, ySS, 18, 18), j*18, k*18, null);
-                    }                    
-                    
-                }
-                //tempLine = "";
+                
+                maze.add(tempLine);
+                
                 k++; //descendo uma linha                
 
             } while ((tempLine = br.readLine()) != null && !tempLine.equals("") );
+            
+            br.close();
 
             } catch (IOException e) {
                     e.printStackTrace();
             } 
         finally {
             try {
-                    if (br != null)br.close();
+                    if (br != null)
+                        br.close();
             } catch (IOException ex) {
                     ex.printStackTrace();
             }
         }
+    }
+    
+    public void update(){
+
+    }
+    
+    public void createObjects(DotController dc){
+        
+        for(int k=0; k < maze.size(); k++){
+
+        tempLine = maze.get(k);
+
+            for(int j = 0; j < 28; j++){
+
+                if(tempLine.substring(2*j, 2*j+2).equals("dd")){
+                    //g2d.drawImage(ss.crop(6, 0, 18, 18), j*18, (k+3)*18, null);
+                    dc.addDot(new Dot(j*18, (k+3)*18));
+                } else if(tempLine.substring(2*j, 2*j+2).equals("up")){
+                    //g2d.drawImage(ss.crop(6, 1, 18, 18), j*18, (k+3)*18, null);
+                } else if(tempLine.substring(2*j, 2*j+2).equals("xx")){
+                    //g2d.drawImage(ss.crop(7, 0, 18, 18), j*18, (k+3)*18, null);
+                } else {
+                    //xSS = Integer.parseInt(tempLine.substring(2*j, 2*j+1));
+                    //ySS = Integer.parseInt(tempLine.substring(2*j+1, 2*j+2));                    
+                    //g2d.drawImage(ss.crop(xSS, ySS, 18, 18), j*18, (k+3)*18, null);
+                }
+
+            }            
+        }    
+    }
+    
+    public void draw(Graphics g2d){
+
+        for(int k=0; k < maze.size(); k++){
+            
+            tempLine = maze.get(k);
+            
+            for(int j = 0; j < 28; j++){
+                                
+                if(tempLine.substring(2*j, 2*j+2).equals("dd")){
+                    //g2d.drawImage(ss.crop(6, 0, 18, 18), j*18, (k+3)*18, null); //6,0
+                } else if(tempLine.substring(2*j, 2*j+2).equals("up")){
+                    g2d.drawImage(ss.crop(6, 1, 18, 18), j*18, (k+3)*18, null); //6,1
+                } else if(tempLine.substring(2*j, 2*j+2).equals("xx")){
+                    g2d.drawImage(ss.crop(7, 0, 18, 18), j*18, (k+3)*18, null); //7,0
+                } else {
+                    xSS = Integer.parseInt(tempLine.substring(2*j, 2*j+1));
+                    ySS = Integer.parseInt(tempLine.substring(2*j+1, 2*j+2));                    
+                    g2d.drawImage(ss.crop(xSS, ySS, 18, 18), j*18, (k+3)*18, null);
+                }
+
+            }            
+        }
+
     }
     
 }
